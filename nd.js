66a49ca126ff0c183cda485a1096e0c8b89dbd40
germanas7 <germanas.motiejauskis@live.com>
2022-12-29 00:53:45 +0200

    number

--- nd.js
+++ nd.js
@@ -1,6 +1,9 @@
 function Skaiciuoti() {
+    event.preventDefault();
     const data = document.getElementById("data").value;
     console.log(data);
     const gimtadienis = document.getElementById("gimtadienis").value;
-    console.log(gimtadienis);}
-    document.getElementById("results").innerHTML =`dabar man yra, ${'data' - 'gimtadienis'}`;
+    console.log(gimtadienis);
+    }
+    document.getElementById("demo").innerHTML = `dabar man yra, ${"data" - "gimtadienis"}`;
+   
