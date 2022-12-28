d1a55dc681abf97b71f0735577a12c66bee79c07
germanas7 <germanas.motiejauskis@live.com>
2022-12-28 15:42:24 +0200

    papildymas

--- nd.js
+++ nd.js
@@ -1,7 +1,9 @@
 const data = document.querySelector("data");
 console.log(data);
 const gimtadienis = document.querySelector("gimtadienis")
-console.log(gimtadienis)
+console.log(gimtadienis);
+const button = document.querySelector('.btn');
+console.log(button);
 document.getElementById("demo").innerHTML =`dabar man yra, ${data - gimtadienis}`;
 
 
